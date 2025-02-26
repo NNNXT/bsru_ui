0acb9de829316cc403e7c7ab6515896dd4388a19
NNNXT <74697758+NNNXT@users.noreply.github.com>
2025-02-26 14:17:32 +0700

    :necktie: add logic for display bottom tab

--- lib/page4.dart
+++ lib/page4.dart
@@ -1,13 +1,44 @@
+import 'package:bsru_ui/widget_home.dart';
 import 'package:flutter/material.dart';
 
-class Page4 extends StatelessWidget {
+class Page4 extends StatefulWidget {
   const Page4({super.key});
+
+  @override
+  State<Page4> createState() => _Page4State();
+}
+
+class _Page4State extends State<Page4> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    WidgetHome(),
+    Text('Index 1: Chat', style: optionStyle),
+    Text('Index 2: Setting', style: optionStyle),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('OpenTalk'),
+      ),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
     );
   }
---
+++ lib/widget_home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WidgetHome extends StatelessWidget {
+  const WidgetHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text('Home'),
+    );
+  }
+}
