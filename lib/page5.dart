acde03e3d240f4880a73ce2b71fbc3071b7ad8c1
NNNXT <74697758+NNNXT@users.noreply.github.com>
2025-02-26 14:41:28 +0700

    :lipstick: add ui for detail user

--- lib/page5.dart
+++ lib/page5.dart
@@ -13,6 +13,35 @@ class Page5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(150),
+              child: Image.network(
+                id,
+                width: 150,
+                height: 150,
+                fit: BoxFit.fill,
+              ),
+            ),
+            Text(name),
+            Text(gender),
+            gender == 'Male'
+                ? const Icon(
+                    Icons.male,
+                    color: Colors.blue,
+                  )
+                : const Icon(
+                    Icons.female,
+                    color: Colors.pink,
+                  ),
+          ],
+        ),
+      ),
+    );
   }
 }
