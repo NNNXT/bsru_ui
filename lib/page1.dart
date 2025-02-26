a126ee06cca1053f05796a31719dfb3e482f05fc
NNNXT <74697758+NNNXT@users.noreply.github.com>
2025-02-26 13:30:34 +0700

    :lipstick: update ui page1

--- lib/page1.dart
+++ lib/page1.dart
@@ -5,6 +5,32 @@ class Page1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FlutterLogo(size: 128),
+            Text(
+              'ยินดีต้อนรับสู่ OPENTALK',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'พบ พูดคุย และแลกเปลี่ยนข้อมูลข่าวสารกับกลุ่มเพื่อน\nที่ชอบอะไรเหมือนๆ กัน',
+              style: TextStyle(fontSize: 14),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('เข้าสู่ระบบ'),
+        ),
+      ),
+    );
   }
 }
